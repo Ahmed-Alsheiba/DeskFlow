b75d9f80496ab3b810553b352ae46e00ed930155
Ahmed-AlShaiba <amashaiba@gmail.com>
2026-06-11 14:51:56 +0400

    Add Rails health check route for kamal-proxy, and change change deploy DB_HOST

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "up" => "rails/health#show", as: :rails_health_check
   devise_for :users
   root "pages#landing"
   # Use plural resource name so form_with(model: @ticket) resolves to tickets_path
