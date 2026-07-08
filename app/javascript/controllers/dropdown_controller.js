12343b4506e8ef37c464efc3d312afc0a6cc6514
Ahmed-AlShaiba <amashaiba@gmail.com>
2026-07-08 20:49:14 +0400

    Optimized UI on samller screens phone/tablets

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -5,16 +5,24 @@ export default class extends Controller {
   static values = { open: { type: Boolean, default: false } }
 
   connect() {
-    document.addEventListener("click", this.handleClickOutside.bind(this))
+    this.boundClickOutside = this.handleClickOutside.bind(this)
+    document.addEventListener("click", this.boundClickOutside)
   }
 
   disconnect() {
-    document.removeEventListener("click", this.handleClickOutside.bind(this))
+    document.removeEventListener("click", this.boundClickOutside)
   }
 
   toggle() {
     this.openValue = !this.openValue
     this.menuTarget.classList.toggle("hidden", !this.openValue)
+    this.syncAria()
+  }
+
+  close() {
+    this.openValue = false
+    this.menuTarget.classList.add("hidden")
+    this.syncAria()
   }
 
   selectOption(event) {
@@ -25,16 +33,18 @@ export default class extends Controller {
     this.hiddenTarget.value = value
     this.buttonTarget.querySelector('[data-selected-label]').textContent = label
 
-    this.openValue = false
-    this.menuTarget.classList.add("hidden")
+    this.close()
   }
 
   handleClickOutside(event) {
     if (!this.element.contains(event.target)) {
-      this.openValue = false
-      this.menuTarget.classList.add("hidden")
+      this.close()
+    }
+  }
+
+  syncAria() {
+    if (this.hasButtonTarget) {
+      this.buttonTarget.setAttribute("aria-expanded", this.openValue)
     }
   }
 }
-
-
